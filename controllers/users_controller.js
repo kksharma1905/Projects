8f961c1fd9ab002bad50573de5b7c83313940728
kk <kksharma1905@yahoo.com>
2019-09-20 18:24:21 +0530

    Manul Local Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                   title: "User Profile",
+                   user: user 
+                });
+            }else{
+                return res.redirect('/users/sign-in');
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 module.exports.signUpIn = function(req, res){
@@ -40,5 +51,30 @@ module.exports.create = function(req, res){
 
 //Sign IN and create sesison
 module.exports.createSession = function(req, res){
+    //Steps to Autheticate
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding User in Signing In'); 
+            return;
+        }
+        //handle user found
+        if(user){
+        //handle password not match
+              if(user.password != req.body.password){
+                    return res.redirect('back');
+                }
+                //handle session cretion
+                 console.log('User Logged in Succesfully');
+                res.cookie('user_id', user.id);
+                return res.redirect('/users/profile');
+        
+        }else{
+            //handler user not found
+            return res.redirect('back');
+        }
+    });
+    //handle password which don't match
 
+    //handle user not found 
 }
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
-router.get('/sign-In', usersController.signUpIn);
+router.get('/sign-in', usersController.signUpIn);
 router.get('/profile', usersController.profile);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
 
 module.exports = router;
